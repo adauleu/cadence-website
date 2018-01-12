0da1d1ee75d2672bc0e6656edcc64e6d5fc3ea0b
dwafotapa <dominique.wafotapa@gmail.com>
2018-01-12 19:07:15 +0100

    Change meta tag prop config

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,11 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Cadence Avocats est né du désir de mettre en commun des compétences complémentaires, au service des entreprises.' },
-      { name: 'og:title', content: 'Cadence Avocats' },
-      { name: 'og:description', content: 'Cadence Avocats est né du désir de mettre en commun des compétences complémentaires, au service des entreprises.' },
-      { name: 'og:type', content: 'website' },
-      { name: 'og:url', content: 'http://cadenceavocats.com/' },
-      { name: 'og:image', content: 'http://cadenceavocats.com/_nuxt/img/Logo-Header.png' }
+      { property: 'og:title', content: 'Cadence Avocats' },
+      { property: 'og:description', content: 'Cadence Avocats est né du désir de mettre en commun des compétences complémentaires, au service des entreprises.' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'http://cadenceavocats.com/' },
+      { property: 'og:image', content: 'http://cadenceavocats.com/_nuxt/img/Logo-Header.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
